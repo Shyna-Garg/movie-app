6b1616c5cabd9ba11a3fa7dc6fa61b35df3f96b3
Shyna-Garg <shynugrg571@gmail.com>
2025-06-29 22:04:01 +0530

    Message

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,22 +1,16 @@
-import { Link } from 'react-router'
-import "../css/NavBar.css"
-const NavBar = () => {
-  return (
-    <div className='navbar'>
-        <div className='navbar-brand'>
-   
-   <Link to="/">Movie App</Link>
-   
-           </div>
+import { Link } from "react-router-dom";
+import "../css/Navbar.css"
 
-           <div className='navbar-links'>
-            <Link to="/" className='nav-link'>Home</Link>
-            <Link to="/favorites" className='nav-link'>Favorites</Link>
-
-           </div>
-
-    </div>
-  )
+function NavBar() {
+    return <nav className="navbar">
+        <div className="navbar-brand">
+            <Link to="/">Movie App</Link>
+        </div>
+        <div className="navbar-links">
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/favorites" className="nav-link">Favorites</Link>
+        </div>
+    </nav>
 }
 
 export default NavBar
--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,16 @@
 const API_KEY = "f27b47ebb9380ff00d7b6160dcb81095";
 const BASE_URL = "https://api.themoviedb.org/3";
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const response = await fetch(`${CORS_PROXY}${BASE_URL}/movie/popular?api_key=${API_KEY}`);
   const data = await response.json();
   return data.results;
 };
 
 export const searchMovies = async (query) => {
   const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+    `${CORS_PROXY}${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
   );
   const data = await response.json();
   return data.results;
